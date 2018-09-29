365a1c49c58ac7b6129267e90b76efdca597459f
rakslice <rakslice@gmail.com>
2018-09-29 03:16:00 -0700

    got nautilus running, probably needs additional components to be useful

---
+++ onbox/nautilus.bash
@@ -0,0 +1,14 @@
+source ./build_thing.bash
+
+#source ${script_path}/gnome_resolver.bash
+
+wtcmmi https://downloads.sourceforge.net/libexif/libexif-0.6.21.tar.bz2 27339b89850f28c8f1c237f233e05b27 LDFLAGS="-R/usr/local/lib -R/usr/tgcware/lib -L/usr/local/lib" CPPFLAGS=-I/usr/local/include
+#wtcmmi https://sourceforge.net/projects/libexif/files/OldFiles/libexif-0.5.12.tar.gz a6794698cb32b5575472b2c2169a68b2bedc7a3d LDFLAGS="-R/usr/local/lib -R/usr/tgcware/lib -L/usr/local/lib" CPPFLAGS=-I/usr/local/include
+
+wtcmmi https://ftp.gnome.org/pub/gnome/sources/gail/0.16/gail-0.16.tar.bz2 002d03875f46af66f8be41cb58b4ca9e LDFLAGS=-R/usr/local/lib -R/usr/tgcware/lib -L/usr/local/lib CPPFLAGS=-I/usr/local/include
+wtcmmi https://ftp.gnome.org/pub/gnome/sources/eel/2.10/eel-2.10.1.tar.bz2 8b01ef585826b606190cc2c2ce836216c456940aeefac0d0aadd093716d94557 LDFLAGS=-R/usr/local/lib -R/usr/tgcware/lib -L/usr/local/lib CPPFLAGS=-I/usr/local/include
+wtcmmi https://ftp.gnome.org/pub/gnome/sources/startup-notification/0.9/startup-notification-0.9.tar.bz2 61de1aed379ea9d44a835baf76567e0aaa813b6d6e995db7e83e8986f69b580d LDFLAGS=-R/usr/local/lib -R/usr/tgcware/lib -L/usr/local/lib CPPFLAGS=-I/usr/local/include
+wtcmmi https://ftp.gnome.org/pub/gnome/sources/nautilus/2.9/nautilus-2.9.92.tar.bz2 4e0593df034b06eb080507f72ca46251ebeb0fce18fedf953cea64c7a83deae4 LDFLAGS=-R/usr/local/lib -R/usr/tgcware/lib -L/usr/local/lib CPPFLAGS=-I/usr/local/include
+
+#wtcmmi http://ftp.gnome.org/pub/gnome/sources/nautilus/2.10/nautilus-2.10.1.tar.bz2 3bf0841e8d53ab5014807fc85eef9b16f077453910d2168c5baa747b4ec7201e LDFLAGS="-R/usr/local/lib -R/usr/tgcware/lib -L/usr/local/lib" CPPFLAGS=-I/usr/local/include
+
